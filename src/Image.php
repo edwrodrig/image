a956e7441e1af0a5afbbfbb9bc40c2a7cc697894
Edwin Rodríguez <edwrodrig@gmail.com>
2018-05-03 00:13:08 -0300

    Working

--- src/Image.php
+++ src/Image.php
@@ -1,11 +1,4 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: edwin
- * Date: 26-03-18
- * Time: 15:06
- */
-
 namespace edwrodrig\image;
 
 use Imagick;
@@ -18,6 +11,18 @@ class Image
         return $return_var == 0;
     }
 
+    /**
+     * Create a super low sized thumbnail.
+     *
+     * This thumbnail is in grayscale, very blurry and in very bad quality.
+     * The purpose of this thumbnail is to create a hint of the complete image that can be stored in a column in a database.
+     * Generally has <1Kb size.
+     * @param string $filename
+     * @param int $columns
+     * @param int $rows
+     * @return Imagick
+     * @throws \ImagickException
+     */
     public static function create_thumbnail(string $filename, int $columns, int $rows) : Imagick {
         $img = new Imagick($filename);
         $img->setImageFormat('jpeg');
@@ -33,38 +38,79 @@ class Image
     }
 
 
-    public static function optimize(string $filename, int $size_hint = 1000) : Imagick
+    /**
+     * Creates an optimized version of a file for web.
+     * @param string $filename
+     * @param int $svg_width Set the width of the image in the svg. This need to be high or it will be pixelated.
+     * @return Imagick
+     * @throws \ImagickException
+     */
+    public static function optimize(string $filename, int $svg_width = 1000) : Imagick
     {
-        $img = new Imagick();
-        $img->setBackgroundColor(new \ImagickPixel("transparent"));
-        $img->readImage($filename);
         $type = mime_content_type($filename);
 
         if ($type === 'image/png') {
-            $img->stripImage();
+            return self::optimize_png($filename);
         } else if ($type === 'image/jpeg') {
-            $img->setSamplingFactors(['2x2', '1x1', '1x1']);
-            $img->setImageCompressionQuality(75);
-            $img->stripImage();
+            return self::optimize_jpg($filename);
         } else if ($type === 'image/svg+xml' ) {
-            $tempnam_in = tempnam("/tmp", "IN");
-            $tempnam_out = tempnam('/tmp', "OUT");
-            file_put_contents($tempnam_in, file_get_contents($filename));
-
-            passthru(sprintf("rsvg-convert %s -f png --keep-aspect-ratio -w %s -o %s", $tempnam_in, $size_hint, $tempnam_out));
-
-            $img = new Imagick();
-            $img->setBackgroundColor("transparent");
-            $img->readImage($tempnam_out);
-
-            $img->setImageBackgroundColor('transparent');
-            $img->borderImage('transparent', $img->getImageWidth() + 2, $img->getImageHeight() + 2);
-            $img->trimImage(0);
+            return self::optimize_svg($filename, $svg_width);
+        } else {
+            throw new exception\WrongFormatException($filename);
         }
+    }
+
+    public static function optimize_png(string $filename) {
+        $img = new Imagick();
+        $img->setBackgroundColor(new \ImagickPixel("transparent"));
+        $img->readImage($filename);
+        $img->stripImage();
 
         return $img;
     }
 
+    public static function optimize_jpg(string $filename) {
+        $img = new Imagick();
+        $img->setBackgroundColor(new \ImagickPixel("transparent"));
+        $img->readImage($filename);
+        $img->setSamplingFactors(['2x2', '1x1', '1x1']);
+        $img->setImageCompressionQuality(75);
+        $img->stripImage();
+        return $img;
+    }
+
+    /**
+     * Returns a trimmed version of the image trimmed to transparent pixels
+     * @param string $filename
+     * @param int $svg_width
+     * @return Imagick
+     * @throws \ImagickException
+     */
+    public static function optimize_svg(string $filename, int $svg_width = 1000) {
+        $tempnam_in = tempnam(sys_get_temp_dir(), "IN");
+        $tempnam_out = tempnam(sys_get_temp_dir(), "OUT");
+        file_put_contents($tempnam_in, file_get_contents($filename));
+
+        //need to create a png output of the file
+        passthru(sprintf("rsvg-convert %s -f png --keep-aspect-ratio -w %s -o %s", $tempnam_in, $svg_width, $tempnam_out));
+
+        $img = new Imagick();
+        $img->setBackgroundColor("transparent");
+        $img->readImage($tempnam_out);
+
+        $img->setImageBackgroundColor('transparent');
+        //to trim transparent area of the image add a transparent border and then trim it
+        $img->borderImage('transparent', $img->getImageWidth() + 2, $img->getImageHeight() + 2);
+        $img->trimImage(0);
+        return $img;
+    }
+
+    /**
+     * @param Imagick $img
+     * @param $color
+     * @return Imagick
+     * @throws \ImagickException
+     */
     public static function color_overlay(Imagick $img, $color) {
         $overlay = new \Imagick();
         $overlay->newImage($img->getImageWidth(), $img->getImageHeight(), $color);
@@ -72,6 +118,18 @@ class Image
         return $overlay;
     }
 
+
+    /**
+     * Makes the image to cover a rectangle.
+     *
+     * The dimension that exceeds the rectangle area will be centered.
+     * If the width or height are 0 makes to scale the image according the defined dimension
+     * @param Imagick $img
+     * @param int $width
+     * @param int $height
+     * @return Imagick
+     * @throws \ImagickException
+     */
     public static function cover(Imagick $img, int $width, int $height) {
         if ( $width == 0 || $height == 0 ) {
             $img->scaleImage($width, $height);
@@ -102,6 +160,18 @@ class Image
         return $img;
     }
 
+    /**
+     * Makes the image to be contained in a rectangle.
+     *
+     * The dimension that does not exceed the rectangle area will be centered.
+     * @param Imagick $img
+     * @param int $width
+     * @param int $height
+     * @param string $background_color
+     * @return Imagick
+     * @throws \ImagickException
+     * @throws exception\InvalidSizeException
+     */
     public static function contain(Imagick $img, int $width, int $height, $background_color = 'transparent') {
         if ( $width == 0 || $height == 0 ) {
             throw new exception\InvalidSizeException($width, $height);
